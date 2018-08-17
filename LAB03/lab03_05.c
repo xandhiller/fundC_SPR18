0934fdb4c0f4d4f3d9b9a1ac0b2661747526f56f
xandhiller <xandhiller@gmail.com>
2018-08-17 20:09:48 +1000

    Made trapezium function.

--- LAB03/lab03_05.c
+++ LAB03/lab03_05.c
@@ -3,20 +3,20 @@
 void printUpTriangle(int height);
 void printRectangle(int length, int width);
 void printDownTriangle(int height);
+void printTrapezium(int side, int middle);
 
 int main (void) {
 
-  printUpTriangle(4);
-  printRectangle(2, 4);
-  printDownTriangle(4);
+  printTrapezium(16, 16);
 
   return 0;
 }
 
 void printUpTriangle(int height) {
+  int i; 
   int k = 0;
-  for (int i = 0; k < height; i++) {
-    for (int i = 0; i<= k; i++) {
+  for (i = 0; k < height; i++) {
+    for (i = 0; i<= k; i++) {
       printf("* ");
     }
     k++;
@@ -25,9 +25,10 @@ void printUpTriangle(int height) {
 }
 
 void printDownTriangle(int height) {
-  int k = height;
-  for (int i = 0; i < height; i++) {
-    for (int i = 0; i< k; i++) {
+  int i, j;  
+  int k = height;  
+  for (i = 0; i < height; i++) {
+    for (j = k; j>0; j--) {
       printf("* ");
     }
     k--;
@@ -36,10 +37,18 @@ void printDownTriangle(int height) {
 }
 
 void printRectangle(int length, int width) {
-  for (int i = 0; i < length; i++) {
-    for (int i = 0; i< width; i++) {
+  int i;
+  for (i = 0; i < length; i++) {
+    for (i = 0; i< width; i++) {
       printf("* ");
     }
     printf("\n");
   }
+}
+
+
+void printTrapezium(int side, int middle) {
+  printUpTriangle(side);
+  printRectangle(middle/2, middle); 
+  printDownTriangle(side);
 }
