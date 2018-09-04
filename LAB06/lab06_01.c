29c62fd5da40e80c34fa3008a433d417e07e45cd
xandhiller <xandhiller@gmail.com>
2018-09-04 10:05:44 +1000

    Separated out task 0 with task 1. Task 0 and task 1 complete.

--- LAB06/lab06_01.c
+++ LAB06/lab06_01.c
@@ -4,22 +4,20 @@ int middle1(int a[], int n);
 int middle2(int* a, int n);
 
 int main (void) {
-  int a[] = {1,2,3,4,5};
-  
-  printf("Middle 1 returns: %d\n", middle1(a, 5));
+  int a = 1, b = 2; 
+  int* ptr = &b; 
 
-  printf("Middle 2 returns: %d\n", middle2(a, 5)); 
+  printf("Pointer address of b from ptr is: %p\n", ptr);
+  printf("Pointer address of b from &b is: %p\n", &b);
+  printf("Pointer address of a is: %p\n", &a);
+  printf("Address of ptr is: %p\n", &ptr);
+  printf("Size of ptr is: %lu\n", sizeof(ptr));
+  char c = 'c';
+  printf("Size of char is: %lu\n", sizeof(c));
+
+  
 
   return 0;  
 }
-
-int middle1(int a[], int n) {
-    return a[n/2];
-}
-
-int middle2(int *a, int n) {
-    return a[n/2];
-}
-
 
 
